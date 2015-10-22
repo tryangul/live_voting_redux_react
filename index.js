1a93aa143a6e970904178fd5a8c315c0ea45b659
tryangul <ryan.broughan@gmail.com>
2015-10-22 13:42:24 -0700

    server broadcasts state and receives actions from client

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ import makeStore from "./src/store";
 import startServer from "./src/server";
 
 export const store = makeStore();
-startServer();
+startServer(store);
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require("./entires.json")
+});
+store.dispatch({type: 'NEXT'});
